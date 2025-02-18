fd934a584590205cbd0189473e7bcb7ff72b69dd
Mateusz <mateusz@tulip.lol>
2025-02-18 18:09:36 +0100

    uart tx base wuth character sending added

---
+++ uart_tx_c/Main.c
@@ -0,0 +1,53 @@
+//Author mztulip
+//mateusz@tulip.lol
+#include "nrf51.h"
+#include "nrf51_bitfields.h"
+
+const uint32_t LED1 = 21; //P0.21
+const uint32_t txd_pin = 9;//P.09 TXD
+
+void delay(void)
+{
+    for (volatile uint32_t i = 0; i < 123456; ++i)
+    {
+
+    }
+}
+
+int main()
+{
+	NRF_GPIO->PIN_CNF[LED1] = ((uint32_t)GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos);
+
+
+	NRF_GPIO->OUTSET = (1<<txd_pin);
+	NRF_GPIO->PIN_CNF[txd_pin] = ((uint32_t)GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos)
+						| ((uint32_t)GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos);
+						
+
+	NRF_UART0->BAUDRATE = 0x01D7E000; //115200
+	NRF_UART0->CONFIG = UART_CONFIG_PARITY_Excluded << UART_CONFIG_PARITY_Pos | UART_CONFIG_HWFC_Disabled;
+	NRF_UART0->PSELTXD = txd_pin;
+	NRF_UART0->ENABLE = UART_ENABLE_ENABLE_Enabled;
+	NRF_UART0->TASKS_STARTTX = 1;
+	while(1)
+	{
+		delay();
+		NRF_GPIO->OUTCLR = (1<<LED1);
+		delay();
+		NRF_GPIO->OUTSET = (1<<LED1);
+		NRF_UART0->TXD = 'a';
+
+	}
+}
+
+void Default_Handler(void)
+{
+	while(1);
+}
